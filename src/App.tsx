f635237ffac5a0d35b735033f0361f69c944d281
kazue kitamura <73727763+kazuekitamura@users.noreply.github.com>
2021-08-07 17:20:55 +0900

    Initial commit

---
+++ src/App.tsx
@@ -0,0 +1,16 @@
+import { Practice1 } from "./practices/practice1";
+import { Practice2 } from "./practices/practice2";
+import { Practice3 } from "./practices/practice3";
+import { Practice4 } from "./practices/practice4";
+import "./styles.css";
+
+export default function App() {
+  return (
+    <div className="App">
+      <Practice1 />
+      <Practice2 />
+      <Practice3 />
+      <Practice4 />
+    </div>
+  );
+}
---
+++ src/typeList.ts
@@ -0,0 +1,25 @@
+/* eslint-disable @typescript-eslint/no-unused-vars */
+// boolean
+let bool: boolean = true;
+// number数値
+let num: number = 0;
+//　string 文字
+let str: string = "A";
+// Array配列
+let arr1: Array<number> = [0, 1, 2];
+let arr2: number[] = [0, 1, 2];
+//tuple
+let tuple: [number, string] = [0, "A"];
+//any
+let any1: any = false;
+//void(何も返却してない)書かなくてもいい
+const funcA = (): void => {
+  const test = "TEST";
+};
+//null
+let null1: null = null;
+//undefind
+let undefind1: undefined = undefined;
+//object
+let obj: object = {};
+let obj2: { id: number; name: string } = { id: 0, name: "AAA" };
